155ac87a2c2d61cc668da957a8bffa29970d230f
Himanshu Shukla <36098036+himanshushkl691@users.noreply.github.com>
2019-01-07 20:20:39 +0530

    Add files via upload

--- ASSIGNMENT5/ALU.v
+++ ASSIGNMENT5/ALU.v
@@ -1,11 +1,14 @@
-module ALU(out,s1,s0,x,y);
+module ALU(out,s,x,y);
    input[31:0] x,y;
    output [31:0] out;
-   input 	 s1,s0;
+   input 	 s;
    wire [31:0] 	 in0,in1,in2;
-   MULTIPLIER mult0(in0,x,y);
+   ADDER_32BIT add0(in0,,x,y,0);
+   SUBTRACTOR sub0(in1,,x,y,0);
+   /*MULTIPLIER mult0(in0,x,y);
    DIV_REM divrem0(in1,in2,x,y);
-   MUX3X1_32BIT mux0(out,s1,s0,in0,in1,in2);
+   MUX3X1_32BIT mux0(out,s1,s0,in0,in1,in2);*/
+   MUX2X1_32BIT mux0(out,s,in0,in1);
 endmodule // ALU
 
 
